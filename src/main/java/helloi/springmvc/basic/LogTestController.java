cc1e6d3c6134d50fe7f719dca5dafce3a674d22e
sky6min <sky6min@gmail.com>
2021-08-17 22:02:18 +0900

    Initial commit - Spring-mvc basic

---
+++ src/main/java/helloi/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package helloi.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Controller // 문자를 return 하면 view 이름을 반환하는걸로 앎
+@RestController    // RestController로 문자를 return 하면, 문자 그자체를 return 함.
+public class LogTestController {
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.trace("trace log={}", name);
+        log.info(" info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+
+}
